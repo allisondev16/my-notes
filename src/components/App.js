d460576973b288af981dc8fe15c202b53c83aadf
allisondev16 <allisonmamangun@gmail.com>
2021-11-04 23:13:02 +0800

    Update TextArea

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Header from './Header';
+import TextArea from './TextArea';
 
 function App() {
   return (
     <div>
       <Header />
+      <TextArea />
     </div>
   );
 }
---
+++ src/components/TextArea.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TextArea() {
+    return (
+        <form class="create-note">
+            <input></input>
+            <textarea></textarea>
+        </form>
+    )
+}
+
+export default TextArea;
