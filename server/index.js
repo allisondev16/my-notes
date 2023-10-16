939d0a6d4a844d3afbc5f037cccf019978387089
allisondev16 <allisonmamangun@gmail.com>
2023-10-16 22:24:51 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('./database');
-//const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
@@ -14,11 +14,19 @@ const app = express();
 // ));
 //app.use(cors())
 // app.options('*', cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 
 app.use(express.json());
